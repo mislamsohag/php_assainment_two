b16406934d78c931ce7acef90287e0749ac66a37
SohagTech <mislamsohag@gmail.com>
2023-09-20 01:30:24 +0600

    Task3 Done

---
+++ task3.php
@@ -0,0 +1,29 @@
+
+<?php
+/*
+Task 3: Break on Condition
+Write a PHP program that calculates and prints the first 10 Fibonacci numbers. But, if a Fibonacci number is greater than 100, break out of the loop using the break statement.
+*/
+
+function fibonacci($number){
+    
+    if($number==0){
+        return 0;
+    }elseif($number==1){
+        return 1;
+    }else{
+        return (fibonacci($number-1)+fibonacci($number-2));
+    }
+}
+
+$fiboNumber=12;
+for($i=0; $i<=$fiboNumber; $i++){
+    
+    $fiboResult=fibonacci($i);
+    if($fiboResult>100){
+        break;
+    }else{
+        echo $fiboResult ," ";
+    }
+    
+}
